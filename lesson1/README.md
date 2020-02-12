e15a4d15f60d82076aa8d8bf8d1e05baafdcac16
PresByter <presbyter163@163.com>
2020-02-12 16:51:03 +0800

    py generate pdf

--- lesson1/README.md
+++ lesson1/README.md
@@ -1,1 +1,15 @@
 # python 练手
+
+# 环境配置
+```bash
+pipenv install
+```
+To activate this project's virtualenv, run pipenv shell.
+Alternatively, run a command inside the virtualenv with pipenv run.
+# 参考文档
+- [pdfkit  wkhtmltopdf](https://www.cnblogs.com/RainBol/p/10881194.html)
+## 生成pdf
+需要安装以下 插件
+- [pdfkit](https://pypi.org/project/pdfkit/)
+- [wkhtmltopdf](https://github.com/JazzCore/python-pdfkit/wiki/Installing-wkhtmltopdf)
+- [wkhtmltopdf下载地址](https://wkhtmltopdf.org/downloads.html)
--- lesson1/topdf.py
+++ lesson1/topdf.py
@@ -1,1 +1,23 @@
+import pdfkit
+
 print('2222')
+
+# pdfkit.from_url('http://google.com', 'out.pdf')
+# pdfkit.from_file('test.html', 'out.pdf')
+# pdfkit.from_string('Hello!', 'out.pdf')
+
+body = """
+    <html>
+      <head>
+        <meta name="pdfkit-page-size" content="Legal"/>
+        <meta name="pdfkit-orientation" content="Landscape"/>
+      </head>
+      Hello World!
+          <img src="http://lixing-develop.oss-cn-hangzhou.aliyuncs.com/large_file/1557201663002.jpg">
+
+      </html>
+    """
+#       http://lixing-develop.oss-cn-hangzhou.aliyuncs.com/large_file/1557201663002.jpg
+# with --page-size=Legal and --orientation=Landscape
+config = pdfkit.configuration(wkhtmltopdf=r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe")
+pdfkit.from_string(body, 'out.pdf', configuration=config)
